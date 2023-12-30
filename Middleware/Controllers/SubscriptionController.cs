18688b82b97eaf37148bfd785b065c9407278bb2
Carlos Franco <33024180+cfranco97@users.noreply.github.com>
2023-12-30 18:54:02 +0000

    added Mqtt package + model + changes to Subscription controller

--- Middleware/Controllers/SubscriptionController.cs
+++ Middleware/Controllers/SubscriptionController.cs
@@ -18,11 +18,10 @@ namespace Middleware.Controllers
 
         #region Gets
 
-        
-
         //Get all subscriptions.
+        [HttpGet]
         [Route("api/somiod/subscriptions")]
-        public IEnumerable<Subscription> GetAll()
+        public IEnumerable<Subscription> GetAllSubscriptions()
         {
             // criar lista vazia de subscriptions.
             List<Subscription> subscriptions = new List<Subscription>();
@@ -42,30 +41,30 @@ namespace Middleware.Controllers
                     Subscription subscription = new Subscription();
                     {
                         subscription.Id = (int)reader["Id"];
-                        subscription.Name = (string)reader["Name"];
-                        subscription.Creation_dt = (DateTime)reader["Creation_dt"];
-                        subscription.Parent = (int)reader["Parent"];
-                        subscription.Event = (string)reader["Event"];
-                        subscription.Endpoint = (string)reader["Endpoint"];
+                        subscription.Name = (string)reader["name"];
+                        subscription.Creation_dt = (DateTime)reader["creation_dt"];
+                        subscription.Parent = (int)reader["parent"];
+                        subscription.Event = (string)reader["event"];
+                        subscription.Endpoint = (string)reader["endpoint"];
                     };
 
                     subscriptions.Add(subscription);
                 }
-
                 reader.Close();
                 conn.Close();
-
+                return subscriptions;
             }
             catch (Exception ex)
             {
                 if (conn.State == System.Data.ConnectionState.Open)
                 {
                     conn.Close();
+                    Debug.Print("[DEBUG] ERROR CONNECTING TO Database");
                 }
+                return subscriptions;
             }
-            return subscriptions;
+            
         }
-
         #endregion
 
         #region CRUDS
@@ -73,7 +72,7 @@ namespace Middleware.Controllers
         // create new subscription.
         [HttpPost]
         [Route("api/somiod/{appName}/{containerName}")]
-        public IHttpActionResult Post([FromBody] string appName, [FromBody] string containerName)
+        public IHttpActionResult Post([FromBody] string appName, [FromBody] string containerName, HttpRequestMessage request)
         {
             // Verificar se application ou container estão guardados na BD.
             string sqlApplication = "SELECT Id FROM Applications WHERE UPPER(Name) = UPPER(@AppName)";
@@ -144,10 +143,102 @@ namespace Middleware.Controllers
                 return InternalServerError();
             }
 
-            // verificar se o XML do request é valido
+            // verificar se o XML do request é valido.
+            HandlerXML handler = new HandlerXML();
+
+            // serializar a string do request
+            string rawXml = request.Content.ReadAsStringAsync().Result.Replace(System.Environment.NewLine, String.Empty);
+            
+            // verificar se o XML do request é valido.
+            if (!handler.IsValidXML(rawXml))
+            {
+                Debug.Print("[DEBUG] 'String is not XML' | Post() in SubscriptionController");
+                return Content(HttpStatusCode.BadRequest, "Request is not XML", Configuration.Formatters.XmlFormatter);
+            }
 
 
-            return null;
+            // TODO:Verificar se o xml está de acordo com o schema.
+
+
+
+            // Criação da nova subscription após as verificações XML serem feitas
+            Subscription subscription = new Subscription();
+            subscription = handler.SubscriptionRequest();
+            subscription.Parent = containerId;
+
+            Debug.Print("[DEBUG] 'Event: " + subscription.Event + " ' | Post() in SubscriptionController");
+
+            // verificação do tipo de evento - creation ou deletion
+            if (!subscription.Event.ToLower().Equals("creation")
+                && !subscription.Event.ToLower().Equals("deletion"))
+            {
+                Debug.Print("[DEBUG] 'Event is invalid' | Post() in SubscriptionController");
+                return Content(HttpStatusCode.BadRequest, "Subscription event is invalid", Configuration.Formatters.XmlFormatter);
+            }
+
+            // verificação do subscription name & endpoint
+            if (String.IsNullOrEmpty(subscription.Name))
+            {
+                subscription.Name = containerName;
+            }
+
+            if (String.IsNullOrEmpty(subscription.Endpoint))
+            {
+                subscription.Endpoint = "127.0.0.1";
+            }
+
+            // verificar previamente na base de dados se já existe um subscription com o mesmo evento.
+            string sqlVerifySubscriptionInContainer = "SELECT COUNT(*) FROM Subscriptions WHERE Parent = @ParentId AND UPPER(Name) = UPPER(@Name) AND (UPPER(Event) = UPPER(@Event)";
+            string sqlPostSubscription = "INSERT INTO Subscriptions(Name, Creation_dt, Parent, Event, Endpoint) OUTPUT INSERTED.Id VALUES (@Name, @Creation_dt, @Parent, @Event, @Endpoint)";
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmdSubscriptionName = new SqlCommand(sqlVerifySubscriptionInContainer, conn);
+                cmdSubscriptionName.Parameters.AddWithValue("@ParentId", containerId);
+                cmdSubscriptionName.Parameters.AddWithValue("@Name", subscription.Name);
+                cmdSubscriptionName.Parameters.AddWithValue("@Event", subscription.Event);
+                cmdSubscriptionName.Parameters.AddWithValue("@Both", "both");
+
+                if ((int)cmdSubscriptionName.ExecuteScalar() > 0)
+                {
+                    conn.Close();
+                    Debug.Print("[DEBUG] 'This subscription name already exists in this module with the same event (" + subscription.Event + ")' | Post() in SubscriptionController");
+                    return Content(HttpStatusCode.BadRequest, "This subscription name already exists in this module with the same event or exists with event 'both'", Configuration.Formatters.XmlFormatter);
+                }
+
+                SqlCommand cmdPost = new SqlCommand(sqlPostSubscription, conn);
+                cmdPost.Parameters.AddWithValue("@Name", subscription.Name);
+                cmdPost.Parameters.AddWithValue("@Creation_dt", DateTime.Now);
+                cmdPost.Parameters.AddWithValue("@Parent", subscription.Parent);
+                cmdPost.Parameters.AddWithValue("@Event", subscription.Event);
+                cmdPost.Parameters.AddWithValue("@Endpoint", subscription.Endpoint);
+
+                // Obter o Id diretamente do comando de inserção
+                subscription.Id = (int)cmdPost.ExecuteScalar();
+
+                conn.Close();
+
+                if (subscription.Id > 0)
+                {
+                    handler.AddSubscription(subscription);
+                    return Content(HttpStatusCode.OK, "Subscription added successfully", Configuration.Formatters.XmlFormatter);
+                }
+
+                return InternalServerError();
+            }
+            catch (Exception e)
+            {
+                Debug.Print("[DEBUG] 'Exception in Post() in SubscriptionController' | " + e.Message);
+
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+
+                return InternalServerError();
+            }
         }
 
 
--- Middleware/XML/HandlerXML.cs
+++ Middleware/XML/HandlerXML.cs
@@ -11,7 +11,35 @@ namespace Middleware.XML
 {
     public class HandlerXML
     {
+        private bool isValid = true;
+        public string XmlFileTempPath { get; set; }
         public string XmlFilePath { get; set; }
+        public string XsdFilePathApplications { get; set; }
+        public string XsdFilePathContainers { get; set; }
+        public string XsdFilePathData { get; set; }
+        public string XsdFileSubscriptions { get; set; }
+        public string XsdFilePathSomiod { get; set; }
+
+        public bool IsValidXML(string input)
+        {
+            // verifica primeiro se a string não vem vazia.
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            try
+            {
+                XmlDocument document = new XmlDocument();
+                document.LoadXml(input);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
 
 
 
@@ -32,6 +60,78 @@ namespace Middleware.XML
 
         #region XML Subscriptions handler
 
+        // Faz tratamento dos dados do request retorna uma nova subscription 
+        public Subscription SubscriptionRequest()
+        {
+            XmlDocument doc = new XmlDocument();
+            Subscription subscription = new Subscription();
+            doc.Load(XmlFileTempPath);
+
+            XmlNode node = doc.SelectSingleNode("//somiod/subscription");
+            if (node.SelectSingleNode("name") != null)
+            {
+                subscription.Name = node.SelectSingleNode("name").InnerText;
+            }
+
+            if (node.SelectSingleNode("event") != null)
+            {
+                subscription.Event = node.SelectSingleNode("event").InnerText;
+            }
+
+            if (node.SelectSingleNode("endpoint") != null)
+            {
+                subscription.Endpoint = node.SelectSingleNode("endpoint").InnerText;
+            }
+
+            RefreshTempFile();
+            return subscription;
+        }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XmlFilePath);
+            XmlNode nodeSubscriptions = doc.SelectSingleNode("//applications/application/containers/container[id='" + subscription.Parent + "']/subscriptions");
+
+            //Inserir tag <containers>
+            if (nodeSubscriptions == null)
+            {
+                nodeSubscriptions = doc.CreateElement("subscriptions");
+                doc.SelectSingleNode("//applications/application/containers/container[id='" + subscription.Parent + "']").AppendChild(nodeSubscriptions);
+            }
+
+            //Inserir tag <container>
+            XmlNode xmlSubscription = doc.CreateElement("subscription");
+
+            XmlNode nodeAux = doc.CreateElement("id");
+            nodeAux.InnerText = subscription.Id.ToString();
+            xmlSubscription.AppendChild(nodeAux);
+
+            nodeAux = doc.CreateElement("creation_dt");
+            nodeAux.InnerText = subscription.Creation_dt.ToString();
+            xmlSubscription.AppendChild(nodeAux);
+
+            nodeAux = doc.CreateElement("name");
+            nodeAux.InnerText = subscription.Name;
+            xmlSubscription.AppendChild(nodeAux);
+
+            nodeAux = doc.CreateElement("parent");
+            nodeAux.InnerText = subscription.Parent.ToString();
+            xmlSubscription.AppendChild(nodeAux);
+
+            nodeAux = doc.CreateElement("event");
+            nodeAux.InnerText = subscription.Event.ToString();
+            xmlSubscription.AppendChild(nodeAux);
+
+            nodeAux = doc.CreateElement("endpoint");
+            nodeAux.InnerText = subscription.Endpoint.ToString();
+            xmlSubscription.AppendChild(nodeAux);
+
+            doc.SelectSingleNode("//applications/application/containers/container[id='" + subscription.Parent + "']/subscriptions").AppendChild(xmlSubscription);
+
+            doc.Save(XmlFilePath);
+        }
+
         public void DeleteSubscription(Subscription subscription)
         {
 
@@ -49,6 +149,25 @@ namespace Middleware.XML
 
             Debug.Print("[DEBUG] 'Subscriptions delete with success' | DeleteSubscription() in HandlerXML");
         }
+
         #endregion
+
+        #region XML handler
+        public void RefreshTempFile()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XmlFileTempPath);
+
+            XmlNode node = doc.SelectSingleNode("//somiod");
+
+            while (node.HasChildNodes)
+            {
+                node.RemoveChild(node.FirstChild);
+            }
+
+            doc.Save(XmlFileTempPath);
+        }
+        #endregion
+
     }
 }
