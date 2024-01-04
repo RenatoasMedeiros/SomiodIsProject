60c5a896a1103f0ec41cb4e4a8e905d9372231fd
Carlos Franco <33024180+cfranco97@users.noreply.github.com>
2024-01-04 17:27:53 +0000

    deleted application.xml and unecessary handler methods

--- Middleware/Controllers/SubscriptionController.cs
+++ Middleware/Controllers/SubscriptionController.cs
@@ -228,7 +228,6 @@ namespace Middleware.Controllers
 
                 if (subscription.Id > 0)
                 {
-                    handler.AddSubscription(subscription);
                     return Content(HttpStatusCode.OK, "Subscription added successfully", Configuration.Formatters.XmlFormatter);
                 }
 
@@ -286,7 +285,6 @@ namespace Middleware.Controllers
 
                 if (numRows > 0)
                 {
-                    handler.DeleteSubscription(subscription);
                     return Content(HttpStatusCode.OK, "Subscription delete successfully", Configuration.Formatters.XmlFormatter);
                 }
                 return Content(HttpStatusCode.BadRequest, "Subscription does not exist", Configuration.Formatters.XmlFormatter);
