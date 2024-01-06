ae61799fa6bf8ed505432e99aee95012e693d00c
ImLuui <72857292+ImLuui@users.noreply.github.com>
2024-01-06 18:43:51 +0000

    Lock Initial

--- TestApplication/Form1.Designer.cs
+++ TestApplication/Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxLock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLock
+            // 
+            this.pictureBoxLock.Image = global::TestApplication.Properties.Resources.Locked;
+            this.pictureBoxLock.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxLock.InitialImage")));
+            this.pictureBoxLock.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxLock.Name = "pictureBoxLock";
+            this.pictureBoxLock.Size = new System.Drawing.Size(547, 323);
+            this.pictureBoxLock.TabIndex = 0;
+            this.pictureBoxLock.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(571, 347);
+            this.Controls.Add(this.pictureBoxLock);
+            this.Name = "Form1";
+            this.Text = "Lock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLock)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLock;
     }
 }
 
--- TestApplication/Form1.cs
+++ TestApplication/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,15 +7,63 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Hosting;
 using System.Windows.Forms;
 
 namespace TestApplication
 {
     public partial class Form1 : Form
     {
+        RestClient client = new RestClient("http://localhost:61552/api/somiod");
+
+        
         public Form1()
         {
+             // 
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var request = new RestRequest("http://localhost:61552/api/somiod/lock", Method.Get);
+            request.RequestFormat = DataFormat.Xml;
+            request.AddHeader("somiod-discover", "application");
+
+            var response = client.Execute(request);
+
+            if (response.Content != "lock")
+            {
+                var xml = @"<application>
+                           <name>lock</name>
+                           <res_type>application</res_type>
+                       </application>";
+
+                request = new RestRequest("http://localhost:61552/api/somiod/lock", Method.Post)
+                {
+                    RequestFormat = DataFormat.Xml
+                };
+
+                request.AddParameter("application/xml", xml, ParameterType.RequestBody);
+
+                response = client.Execute(request);
+
+                pictureBoxLock.Image = Properties.Resources.Unlocked;
+            }
+
+            //MessageBox.Show(response.Content.ToString());
+
+            //SE JA EXISTIR CONTINA A USAR A EXISTENTE SENAO POST
+            //pedido para ver o estado
+            //set da imagem
+
+            //SENAO 
+            //POST CRIAR CADEADO 
+            //pedido para ver o estado
+            //set da imagem
+
+
+
         }
     }
 }
